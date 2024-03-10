89c8cf6f666a051331ceb1788e5f3ef385578168
patshelloworld <162657760+patshelloworld@users.noreply.github.com>
2024-03-10 16:42:12 -0700

    Create unionfind.java

---
+++ unionfind.java
@@ -0,0 +1,22 @@
+//UnionFind
+
+class UnionFind{
+        int[] parent;
+        public UnionFind(int size){
+            parent = new int[size];
+            for(int i = 0; i < size; i++){
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x){
+            if(parent[x] != x){
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public void union(int x, int y){
+            parent[find(x)] = find(y);
+        }
+}
