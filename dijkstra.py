3d368039cfd5f82e33e3248085444b14faa0573e
patshelloworld <162657760+patshelloworld@users.noreply.github.com>
2026-06-24 23:09:20 -0700

    Add Dijkstra's algorithm implementation
    Implement Dijkstra's algorithm for shortest path calculation.

---
+++ dijkstra.py
@@ -0,0 +1,25 @@
+import heapq
+def dijkstra(self, edges:List[List[int]], target:int, source:int):
+    adj = defaultdict(list)
+    #OR adj = [[] for _ in range(n)]
+    for u, v, w in edges:
+        adj[u].append([v,w])
+
+    # piton heap is list, then use heapq.heap...
+    pq = []
+    heapq.heappush(pq, [0, source]) #source is starting position
+
+    dist = {}
+    
+    while pq:
+        weight, node = heapq.heappop(pq)
+        if dist.get(node, math.inf) != math.inf:
+            continue
+        dist[node] = weight
+        if node == target:
+            return weight
+            
+        for u, w in adj[node]:
+            heapq.heappush(pq, [weight+w, u])
+    return -1
+           
